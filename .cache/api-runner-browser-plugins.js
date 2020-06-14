f4a7185fcc7f618742da73bbec95a2dc53e5b22a
mihirat <teketen5656@gmail.com>
2020-06-14 14:18:46 +0700

    remove previous content

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,16 @@
+module.exports = [{
+      plugin: require('/Users/hiratsukamichihisa/src/github.com/mihirat/blog-jp/node_modules/gatsby-plugin-layout/gatsby-browser'),
+      options: {"plugins":[],"component":"/Users/hiratsukamichihisa/src/github.com/mihirat/blog-jp/src/layouts/index.js"},
+    },{
+      plugin: require('/Users/hiratsukamichihisa/src/github.com/mihirat/blog-jp/node_modules/gatsby-plugin-catch-links/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/hiratsukamichihisa/src/github.com/mihirat/blog-jp/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/hiratsukamichihisa/src/github.com/mihirat/blog-jp/node_modules/gatsby-plugin-google-analytics/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/hiratsukamichihisa/src/github.com/mihirat/blog-jp/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
