042ad59c4da655e8292ca40b5dee2afd91f900dd
mihirat <teketen5656@gmail.com>
2020-06-18 22:20:39 +0700

    remove mobileheader bio

--- src/components/header/MobileBio.js
+++ src/components/header/MobileBio.js
@@ -8,7 +8,7 @@ const MobileBio = (props) => {
     return (
         <div className="mobile-bio-main">
             <img src={mihirat}  className="ml-4 mt-2" style={{ maxWidth: `75px`, maxHeight: `75px`, borderRadius: `50%`,boxShadow: `1px 1px 3px`}} alt="author-pic" />
-            <h4 className="mr-4 mt-4">{props.author}</h4>
+            <h4 className="mr-3 mt-3">{props.author}</h4>
         </div>
     )
 }
