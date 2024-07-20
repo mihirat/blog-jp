c724b1a0fa4cd85854e7847cd5711014c04e4d40
Michi Hiratsuka <teketen5656@gmail.com>
2024-07-20 23:46:58 +0900

    astrofied (#58)

---
+++ src/config.ts
@@ -0,0 +1,63 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+export const siteConfig: SiteConfig = {
+  title: 'Teche Note',
+  subtitle: 'ソフトウェアエンジニアの人生備忘録',
+  lang: 'ja',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  themeColor: {
+    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
+  },
+  banner: {
+    enable: true,
+    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center', // Equivalent to object-position, defaults center
+  },
+  favicon: [    // Leave this array empty to use the default favicon
+    // {
+    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    // }
+  ]
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: 'assets/images/mihirat.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'mihirat',
+  bio: '福岡に住んでいるソフトウェアエンジニア',
+  links: [
+    {
+      name: 'Twitter',
+      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+                                        // You will need to install the corresponding icon set if it's not already included
+                                        // `pnpm add @iconify-json/<icon-set-name>`
+      url: 'https://x.com/techeten',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/mihirat',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: false,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
